e9360ddfa0a4daf5694e93ecafc9bf4532323c99
Benjamin Maldonado <benja.ignacio2001@gmail.com>
2024-02-18 16:45:48 -0300

    transformar el archivo a json

--- go/src/main.go
+++ go/src/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"os"
+
+	task "github.com/benjaNygit/cli-crud/go/src/tasks"
 )
 
 func main() {
-	// file := FileMain()
+	file := FileMain()
+	var tasks []task.Task
+
+	info, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// si hay info la transforma en json
+	if info.Size() != 0 {
+		bytes, err := io.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = json.Unmarshal(bytes, &tasks)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		tasks = []task.Task{}
+	}
 }
 
 func FileMain() *os.File {
